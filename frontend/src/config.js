7245d1dc909229ca2e9582aee23ab0cacfd39983
kralfred <alfred.withers.code@gmail.com>
2026-06-24 02:50:27 +0200

    added db schema global variable

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,5 @@
 export const CONFIG = {
-    BACKEND_URL: "http://localhost:8080"
+    BACKEND_URL: window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
+        ? "http://localhost:8080"
+        : "https://amazing-api-460348586740.europe-central2.run.app"
 };
